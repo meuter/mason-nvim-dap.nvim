5ffc1ce259d095cc3a0006a4d9cbdbe6a8068454
Cédric Meuter <cedric.meuter@gmail.com>
2023-04-15 09:38:42 +0200

    use mason-core.path.bin_prefix

--- lua/mason-nvim-dap/mappings/adapters.lua
+++ lua/mason-nvim-dap/mappings/adapters.lua
@@ -1,17 +1,10 @@
-local settings = require('mason-nvim-dap.settings')
+local path = require('mason-core.path')
 
 local M = {}
 
-local function mason_tool_path(tool)
-    -- TOOD: handle '/' in a more cross platform way (maybe use 'mason-code.path' module ?)
-    -- TODO: the path below is the default install path, should query mason for the install path
-    --       in case people configure the path in their mason config
-    return vim.fn.stdpath("data") .. "/mason/bin/" .. tool
-end
-
 M.bash = {
     type = 'executable',
-    command = mason_tool_path('bash-debug-adapter'),
+    command = path.bin_prefix('bash-debug-adapter'),
 }
 
 -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#go-using-delve-directly
@@ -19,46 +12,46 @@ M.delve = {
     type = 'server',
     port = '${port}',
     executable = {
-        command = mason_tool_path('dlv'),
+        command = path.bin_prefix('dlv'),
         args = { 'dap', '-l', '127.0.0.1:${port}' },
     },
 }
 
 M.python = {
     type = 'executable',
-    command = mason_tool_path('debugpy-adapter'),
+    command = path.bin_prefix('debugpy-adapter'),
 }
 
 M.node2 = {
     type = 'executable',
-    command = mason_tool_path('node-debug2-adapter'),
+    command = path.bin_prefix('node-debug2-adapter'),
 }
 
 M.chrome = {
     type = 'executable',
-    command = mason_tool_path('chrome-debug-adapter'),
+    command = path.bin_prefix('chrome-debug-adapter'),
 }
 
 M.firefox = {
     type = 'executable',
-    command = mason_tool_path('firefox-debug-adapter'),
+    command = path.bin_prefix('firefox-debug-adapter'),
 }
 
 M.php = {
     type = 'executable',
-    command = mason_tool_path('php-debug-adapter'),
+    command = path.bin_prefix('php-debug-adapter'),
 }
 
 M.coreclr = {
     type = 'executable',
-    command = mason_tool_path('netcoredbg'),
+    command = path.bin_prefix('netcoredbg'),
     args = { '--interpreter=vscode' },
 }
 
 M.cppdbg = {
     id = 'cppdbg',
     type = 'executable',
-    command = mason_tool_path('OpenDebugAD7'),
+    command = path.bin_prefix('OpenDebugAD7'),
 }
 if vim.fn.has('win32') == 1 then
     M.cppdbg.options = {
@@ -70,7 +63,7 @@ M.codelldb = {
     type = 'server',
     port = '${port}',
     executable = {
-        command = mason_tool_path('codelldb'),
+        command = path.bin_prefix('codelldb'),
         args = { '--port', '${port}' },
     },
 }
@@ -80,19 +73,19 @@ end
 
 M.mix_task = {
     type = 'executable',
-    command = mason_tool_path('elixir-ls-debugger'), -- https://github.com/williamboman/mason.nvim/blob/d97579ccd5689f9c6c365e841ea99c27954112ec/lua/mason-registry/elixir-ls/init.lua#L26
+    command = path.bin_prefix('elixir-ls-debugger'), -- https://github.com/williamboman/mason.nvim/blob/d97579ccd5689f9c6c365e841ea99c27954112ec/lua/mason-registry/elixir-ls/init.lua#L26
     args = {},
 }
 
 M.kotlin = {
     type = 'executable',
-    command = mason_tool_path('kotlin-debug-adapter'),
+    command = path.bin_prefix('kotlin-debug-adapter'),
     args = { '--interpreter=vscode' },
 }
 
 M.dart = {
     type = 'executable',
-    command = mason_tool_path('dart-debug-adapter'),
+    command = path.bin_prefix('dart-debug-adapter'),
     args = { 'flutter' },
 }
 
